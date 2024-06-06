5aa2e6b9d8b79dcb2394f96c324bd5ee8ed8485c
yahiasmeism <yahiasmeism@gmail.com>
2024-06-06 16:05:22 +0300

    create user use cases and finish domin layer

---
+++ lib/features/users/domain/use_cases/delete_user_by_id_use_case.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+
+import 'package:contact_book/core/error/failures.dart';
+import 'package:contact_book/features/users/domain/repositories/user_repository.dart';
+
+class DeleteUserByIdUseCase {
+  UserRepository userRepository;
+  DeleteUserByIdUseCase({
+    required this.userRepository,
+  });
+  Future<Either<Failure, Unit>> call({required String id}) {
+    return userRepository.deleteUserById(id);
+  }
+}

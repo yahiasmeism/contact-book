86ba9493846be50c06b8b8a7c546a9debf5514ac
yahiasmeism <yahiasmeism@gmail.com>
2024-06-24 12:25:54 +0300

    create contactEntity

---
+++ lib/features/contacts/domain/entities/contact.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+import 'package:contact_book/features/company/data/models/company_model.dart';
+
+class ContactEntity {
+  final int id;
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String? emailTwo;
+  final String phoneNumber;
+  final String? mobileNumber;
+  final File? imageUploadFile;
+  final String? imageUrl;
+  final ContactStatus? status;
+  final bool isFavorite;
+  final String address;
+  final String? addressTwo;
+  final CompanyModel companyModel;
+
+  ContactEntity({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.emailTwo,
+    required this.phoneNumber,
+    required this.mobileNumber,
+    required this.imageUploadFile,
+    required this.imageUrl,
+    required this.status,
+    required this.isFavorite,
+    required this.address,
+    required this.addressTwo,
+    required this.companyModel,
+  });
+}
+
+enum ContactStatus { Inactive, Active }
